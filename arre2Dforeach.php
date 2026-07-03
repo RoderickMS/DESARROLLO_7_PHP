925f8f372088a1ef1ccad68b2b237ce8750abae8
RoderickMS <Roderickxams@gmail.com>
2026-07-02 19:55:40 -0500

    Add foreach loop to display student names and grades

--- arre2Dforeach.php
+++ arre2Dforeach.php
@@ -1,1 +1,12 @@
 
+<?php
+$alumnos = [
+    ["nombre" => "Roderick", "nota" => 100],
+    ["nombre" => "Maria", "nota" => 92],
+    ["nombre" => "Juan", "nota" => 80]
+];
+
+foreach ($alumnos as $alumno) {
+    echo  $alumno["nombre"] . "- nota" . $alumno["nota"] . "<br><br>";
+
+}
