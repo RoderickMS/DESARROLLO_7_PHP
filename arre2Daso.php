21d5192a78ce5a7643bc5a557e981603399b852f
RoderickMS <Roderickxams@gmail.com>
2026-07-02 20:52:02 -0500

    Update arre2Daso.php

--- arre2Daso.php
+++ arre2Daso.php
@@ -1,1 +1,21 @@
 
+<?php
+$alumnos = [
+[
+    "nombre" => "Roderick",
+    "nota" => 100,
+    "ciudad" => "David"
+],
+[
+    "nombre" => "Maria",
+    "nota" => 92,
+    "ciudad" => "Panamá"
+],
+[
+    "nombre" => "Juan",
+    "nota" => 80,
+    "ciudad" => "Santiago"
+]
+];
+
+echo $alumnos[0]["nombre"] . " tiene una nota de " . $alumnos[0]["nota"] . " y es de la ciudad de " . $alumnos[1]["ciudad"];
